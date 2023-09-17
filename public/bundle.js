058851dafb86457f4d87c4f42ab2ea246a5d611a
sosamaaa1 <126584200+sosamaaa1@users.noreply.github.com>
2023-09-17 01:18:32 +0000

    ✨Scripts para distribución

---
+++ public/bundle.js
@@ -0,0 +1,1 @@
+console.log(" 🎉 Client Server working powered by Webpackpack 🎉"),function(){let e=arguments.length>0&&void 0!==arguments[0]?arguments[0]:"No message given";console.log(e)}(),async function(){console.log("Calling");const e=await new Promise((e=>{setTimeout((()=>{e("resolved")}),2e3)}));console.log(e)}();
