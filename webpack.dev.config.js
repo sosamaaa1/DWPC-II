2ac736ef8f71ce875964ccc087c1bb39c135c65e
sosamaaa1 <126584200+sosamaaa1@users.noreply.github.com>
2023-09-17 00:41:38 +0000

    ✨Creando archivos de configuración para Webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,8 +3,6 @@ const path = require('path');
 
 // Exportamos un Configuration Options Object
 module.exports = {
-  // 0. Estableciendo el modo produccion
-  mode: 'production',
   // 1. Estableciendo el archivo indexador
   // del front-end
   entry: "./client/index.js",
@@ -16,5 +14,14 @@ module.exports = {
     filename: "bundle.js",
     // 2.3 Ruta base de archivos estaticos
     publicPath: "/"
+  },
+  // 3. Servidor de desarrollo
+  devServer: {
+    // 3.1 Folder de estaticos
+    static: path.join(__dirname, 'public'),
+    // 3.2 Puerto del servidor de desarrollo
+    port: 8080,
+    // 3.3 Definiendo el HOST
+    host: '0.0.0.0'
   }
 }
