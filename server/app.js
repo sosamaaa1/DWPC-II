af49f578895922f6bfb3a695fd89c0841ec7f926
sosamaaa1 <126584200+sosamaaa1@users.noreply.github.com>
2023-10-02 05:07:51 +0000

    ✨ Feature: Webpack Middleware

--- server/app.js
+++ server/app.js
@@ -1,21 +1,60 @@
-//Cargando dependencias
+// Cargando dependencias
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var debug = require('debug')('dwpc-ii:server');
+var debug = require('debug')('dwpcii:server');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-//var testRouter = require('./routes/test');
-// Creando ka instancia de express
+
+// Setting Webpack Modules
+import webpack from 'webpack';
+import WebpackDevMiddleware from 'webpack-dev-middleware';
+import WebpackHotMiddleware from 'webpack-hot-middleware';
+// Importing webpack configuration
+import webpackConfig from '../webpack.dev.config';
+
+// Creando la instancia de express
 var app = express();
 
-// Configurando el motor de plantillas
-//debug("📢Ruta: ${path.join(_dirname, 'views')}");
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Get the execution mode
+const nodeEnviroment = process.env.NODE_ENV || 'production'
 
+// Deciding if we add webpack middleware or not
+if(nodeEnviroment === 'development'){
+  // Start Webpack dev server
+  console.log("🛠️ Ejecutando en modo desarrollo 🛠️");
+  // Adding the key "mode" with its value "development"
+  webpackConfig.mode = nodeEnviroment;
+  // Setting the dev server port to the same value as the express server
+  webpackConfig.devServer.port = process.env.PORT;
+  // Setting up the HMR (Hot Module Replacement)
+  webpackConfig.entry = [
+    "webpack-hot-middleware/client?reload=true&timeout=1000",
+    webpackConfig.entry
+  ];
+	// Agregar el plugin a la configuración de desarrollo
+  // de webpack
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+  // Creating the bundler
+  const bundle = webpack(webpackConfig);
+  // Enabling the webpack middleware
+  app.use( WebpackDevMiddleware(bundle, {
+    publicPath: webpackConfig.output.publicPath
+  }) );
+  //  Enabling the webpack HMR
+  app.use( WebpackHotMiddleware(bundle) );
+}else{
+  console.log("🏭 Ejecutando en modo producción 🏭");
+}
+
+// Configurando el motor de plantillas
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// Se establecen los middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,15 +62,15 @@ app.use(cookieParser());
 // Crea un server de archivos estaticos
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+// Registro de Middlewares de aplicación
 app.use('/', indexRouter);
-
-// Activa usersRouter cuando se 
-//solicita "/users"
+// Activa "usersRourter" cuando se
+// solicita "/users"
 app.use('/users', usersRouter);
-//app.use('/author', (req,res)=>{
-//  res.json({mainDeveloper:"Sosa"})
-//});
-//app.use('/test', testRouter);
+// app.use('/author', (req, res)=>{
+//   res.json({mainDeveloper: "Ivan Rivalcoba"})
+// });
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
