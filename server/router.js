92c02e50ef564a1a8babb615eed65854bba1448d
sosamaaa1 <126584200+sosamaaa1@users.noreply.github.com>
2023-10-15 08:51:36 +0000

    ✨Moving errors to the router

--- server/router.js
+++ server/router.js
@@ -1,3 +1,7 @@
+import createError from 'http-errors';
+// Impornting winston logger
+import log from './config/winston';
+
 // Importando enrutador home
 import homeRouter from './domains/home/home.router';
 
@@ -5,8 +9,27 @@ import homeRouter from './domains/home/home.router';
 const addRoutes = (app) => {
   // Agregando enrutado de Home
   app.use('/', homeRouter);
+
+  // ERRORES
+  // catch 404 and forward to error handler
+  app.use((req, res, next) => {
+    log.info(`404 Pagina no encontrada ${req.method} ${req.originalUrl}`);
+    next(createError(404));
+  });
+
+  // error handler
+  app.use((err, req, res) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    log.error(`${err.status || 500} - ${err.message}`);
+    res.render('error');
+  });
+
   return app;
 };
 
-// Exportando objeto
 export default { addRoutes };
