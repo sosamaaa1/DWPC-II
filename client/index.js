396c288ac94447e3985fdf31232c386a4f371466
sosamaaa1 <126584200+sosamaaa1@users.noreply.github.com>
2023-10-22 09:01:55 +0000

    ✨Improving the Navbar

--- client/index.js
+++ client/index.js
@@ -1,13 +1,12 @@
 // cargando los estilos
-/* eslint-disable no-console */
 import './styles/style.css';
-/* eslint-disable */
-// Importandoe Estilos de Materialize CSS
+// Importando estilos de Materialize CSS
 import 'materialize-css/dist/css/materialize.css';
 // Importando scripts de Materialize
 import 'materialize-css/dist/js/materialize';
-/* eslint-enable */
 
 // Inicializando Scripts de Materialize para interactividad
 M.AutoInit();
-console.log('Webpack Working Middleware!!! 📦');
+
+// Mensaje en la consola
+console.log('🎉 Estilos cargados correctamente 🎉');
