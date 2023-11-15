e8279ee397fe4d684b188b0a198470b504f562b8
sosamaaa1 <126584200+sosamaaa1@users.noreply.github.com>
2023-11-15 16:19:19 +0000

    ✨ Validation template

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -16,7 +16,8 @@ const add = (req, res) => {
 const addPost = (req, res) => {
   // Rescatando la info del formulario
   const { errorData: validationError } = req;
-  // En caso de haber error se le informa al cliente
+  // En caso de haber error
+  // se le informa al cliente
   if (validationError) {
     log.info('Se entrega al cliente error de validación de add Project');
     // Se desestructuran los datos de validación
@@ -29,15 +30,16 @@ const addPost = (req, res) => {
       workingPrev[`${curr.path}`] = curr.message;
       return workingPrev;
     }, {});
-    res.status(422).json({ project, errorModel });
-  } else {
-    // En caso de que pase la validación
-    // Se desestructura la información de la peticion
-    const { validData: project } = req;
-    // Se contesta la información del proyecto al cliente
-    log.info('Se entrega al cliente información del proyecto cargado');
-    res.status(200).json(project);
+    return res.status(422).render('project/addView', { project, errorModel });
   }
+  // En caso de que pase la validación
+  // Se desestructura la información
+  // de la peticion
+  const { validData: project } = req;
+  // Se contesta la información
+  // del proyecto al cliente
+  log.info('Se entrega al cliente información del proyecto cargado');
+  return res.status(200).json(project);
 };
 
 // Controlador user
