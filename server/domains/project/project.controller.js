9a591bc765f78279400989b39be4f1387c5aaec9
sosamaaa1 <126584200+sosamaaa1@users.noreply.github.com>
2023-11-15 15:59:20 +0000

    ✨Validation in views

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -16,18 +16,25 @@ const add = (req, res) => {
 const addPost = (req, res) => {
   // Rescatando la info del formulario
   const { errorData: validationError } = req;
-  // En caso de haber error
-  // se le informa al cliente
+  // En caso de haber error se le informa al cliente
   if (validationError) {
     log.info('Se entrega al cliente error de validación de add Project');
-    res.status(422).json(validationError);
+    // Se desestructuran los datos de validación
+    const { value: project } = validationError;
+    // Se extraen los campos que fallaron en la validación
+    const errorModel = validationError.inner.reduce((prev, curr) => {
+      // Creando una variable temporal para
+      // evitar el error "no-param-reassing"
+      const workingPrev = prev;
+      workingPrev[`${curr.path}`] = curr.message;
+      return workingPrev;
+    }, {});
+    res.status(422).json({ project, errorModel });
   } else {
     // En caso de que pase la validación
-    // Se desestructura la información
-    // de la peticion
+    // Se desestructura la información de la peticion
     const { validData: project } = req;
-    // Se contesta la información
-    // del proyecto al cliente
+    // Se contesta la información del proyecto al cliente
     log.info('Se entrega al cliente información del proyecto cargado');
     res.status(200).json(project);
   }
