455f1245a3e11004f80d287920bf2c4012d08d57
sosamaaa1 <126584200+sosamaaa1@users.noreply.github.com>
2023-11-23 02:25:10 +0000

    ✨Registro e Inición de sesión

--- server/domains/user/user.controller.js
+++ server/domains/user/user.controller.js
@@ -1,19 +1,22 @@
-// Actions methods
+import log from '../../config/winston';
+// Action Methods
 
 // GET '/user/login'
 const login = (req, res) => {
-  res.send("🚧 UNDER CONSTRUCTION GET '/user/login' 🚧");
+  // Sirve el formulario de login
+  log.info('Se entrega formulario de login');
+  res.render('user/login');
 };
+
 // GET '/user/logout'
 const logout = (req, res) => {
-  res.send("🚧 UNDER CONSTRUCTION GET '/user/logout' 🚧");
+  res.send("🚧 UNDER CONSTRUCTION GET  '/user/logout' 🚧");
 };
 
 // GET '/user/register'
 const register = (req, res) => {
-  res.send("🚧 UNDER CONSTRUCTION GET '/user/register' 🚧");
+  res.send("🚧 UNDER CONSTRUCTION GET  '/user/register' 🚧");
 };
-// Controlador Home
 
 export default {
   login,
